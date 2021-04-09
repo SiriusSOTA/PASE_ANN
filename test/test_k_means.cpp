3e5dcf3083ff3b34fdaf1679fe48d8918d1b104a
dmitry <mokeev.dv@phystech.edu>
2021-04-09 16:00:08 +0300

    lil fix + debugging tests

--- test/test_k_means.cpp
+++ test/test_k_means.cpp
@@ -10,20 +10,25 @@
 BOOST_AUTO_TEST_SUITE(CentroidBuild)
 
 BOOST_AUTO_TEST_CASE(test_centroids_build_int) {
-    std::ifstream fin("../../test/test_data/clustering_in.txt"); 
+    std::ifstream fin("../../test/test_data/clustering_in.txt", std::ios_base::in);
     size_t num, dim, x;
-    fin >> num >> dim;
-    std::vector<std::vector<int>> data(num);
-    for (size_t i = 0; i < num; ++i) {
-        for (size_t j = 0; j < dim; ++j) {
-            fin >> x;
-            data[i].push_back(x);
+    if (fin.is_open()) {
+        fin >> num >> dim;
+        std::cout << num << " " << dim << std::endl;
+        std::vector<std::vector<int>> data(num);
+        for (size_t i = 0; i < num; ++i) {
+            for (size_t j = 0; j < dim; ++j) {
+                fin >> x;
+                data[i].push_back(x);
+            }
         }
+        size_t clusterCount, epochs;
+        fin >> clusterCount >> epochs;
+        fin.close();
+        IVFFlatClusterData<int> result = kMeans<int>(data, clusterCount, epochs, 1e-4);
+    } else {
+        throw std::runtime_error("cannot open file");
     }
-    size_t clusterCount, epochs;
-    fin >> clusterCount >> epochs;
-    IVFFlatClusterData<int> result = kMeans<int>(data, clusterCount, epochs, 1e-4);
 }
-
 
 BOOST_AUTO_TEST_SUITE_END()
