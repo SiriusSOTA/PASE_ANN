eb643240a48ab0f5e71847c8691963864228e0fc
yutsareva <tsarovayulia@gmail.com>
2021-04-09 10:40:35 +0300

    Added ids storing

--- test/test_pase_build.cpp
+++ test/test_pase_build.cpp
@@ -8,15 +8,19 @@ BOOST_AUTO_TEST_SUITE(TestPages)
 
     BOOST_AUTO_TEST_CASE(test_fill_dummy_pages)
     {
-        size_t centroidTuplesPerPage = 204;
+        const size_t centroidTuplesPerPage = 204;
+        const size_t dimension = 128;
+
         BOOST_TEST(Page<CentroidTuple<float>>::calcTuplesSize() == centroidTuplesPerPage);
 
-        PaseIVFFlat<float> pase(128, 20);
-        Parser<float> parser("../../test/test_data/siftsmall_base.fvecs", 128, 10000);
+        PaseIVFFlat<float> pase(dimension, 20);
+        Parser<float> parser("../../test/test_data/siftsmall_base.fvecs", dimension, 10000);
         std::vector<std::vector<float>> parsed = parser.parse();
         std::vector<std::reference_wrapper<std::vector<float>>> postparsed(parsed.begin(), parsed.end());
-        for (size_t i = 0; i < centroidTuplesPerPage + 10; ++i) {
-            pase.addCentroid(postparsed, parsed[i]);
+        std::vector<u_int32_t> ids(postparsed.size());
+        std::iota(ids.begin(), ids.end(), 0);
+        for (size_t i = 0; i < centroidTuplesPerPage + 100; ++i) {
+            pase.addCentroid(postparsed, ids, parsed[i]);
         }
 
         BOOST_TEST(pase.firstCentroidPage->tuples[0].vec == parsed[0]);
@@ -28,7 +32,7 @@ BOOST_AUTO_TEST_SUITE(TestPages)
             ++dataPageCount;
         }
 
-        size_t vectorCountPerDataPage = Page<float>::calcTuplesSize() / parsed[0].size();
+        size_t vectorCountPerDataPage = Page<float>::calcVectorCount(dimension);
         size_t lastPageVectorCount = parsed.size() - vectorCountPerDataPage * dataPageCount;
 
         for (size_t i = 0; i < parsed[0].size(); ++i) {
