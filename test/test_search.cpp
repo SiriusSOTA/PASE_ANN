5d6a246104033b957e4de40d062805bf271b6cae
Elena Dudko <dudko.ev@phystech.edu>
2021-04-12 15:59:22 +0300

    update test

--- test/test_search.cpp
+++ test/test_search.cpp
@@ -26,23 +26,30 @@ BOOST_AUTO_TEST_CASE(TestSearch) {
 
     PaseIVFFlat<float> pase(dimension, clusterCount);
     Parser<float> dataParser("../../test/test_data/siftsmall_base.fvecs", dimension, vectorCount);
-    std::vector<const std::vector<float>> dataParsed = dataParser.parse();
-    IVFFlatClusterData<float> clusterData = kMeans<float>(dataParsed, clusterCount, epochs, tol);
+    std::vector<std::vector<float>> dataParsed = dataParser.parse();
+    pase.train(dataParsed, epochs, tol);
 
     Parser<float> testParser("../../test/test_data/siftsmall_query.fvecs", dimension, vectorCount);
-    std::vector<const std::vector<float>> parsedTestData = testParser.parse();
+    std::vector<std::vector<float>> parsedTestData = testParser.parse();
 
     Parser<u_int32_t> answerParser("../../test/test_data/siftsmall_groundtruth.ivecs", nearestVectorsCount, vectorCount);
-    std::vector<const std::vector<u_int32_t>> parsedTestAnswers = answerParser.parse();
+    std::vector<std::vector<u_int32_t>> parsedTestAnswers = answerParser.parse();
     
     Timer t;
+    std::vector<size_t> matchCounter(vectorCount, 0);
     for (size_t i = 0; i < vectorCount; ++i) {
-        std::vector<u_int32_t>& searchVectors = clusterData.findNearestVectorIds(dataParsed[i], nearestVectorsCount, clusterCountToSelect);
+        std::vector<u_int32_t> searchVectors = pase.findNearestVectorIds(dataParsed[i], nearestVectorsCount, clusterCountToSelect);
         for (size_t j = 0; j < nearestVectorsCount; ++j){
-            BOOST_TEST(searchVectors[j] == parsedTestAnswers[i][j]);
+            if (searchVectors[j] == parsedTestAnswers[i][j])
+                matchCounter[i]++;
         }
     }
-    std::cout << "IVFFlat done in " << t.elapsed() << " seconds" << std::endl;
+    size_t time = t.elapsed();
+    std::cout << "Search done in " << time << " seconds" << std::endl;
+    std::cout << "Average running time" << time / vectorCount << std::endl;
+    for (size_t i = 0; i < vectorCount; ++i) {
+        std::cout << "match: " << matchCounter[i] << "out of 100" << std::endl;
+    }
 }
 
 BOOST_AUTO_TEST_SUITE_END()
