f5b6ffa1a97ac58512b3f6f5f5ecac55db0b187c
yutsareva <tsarovayulia@gmail.com>
2021-04-14 15:28:53 +0300

    Added scripts for building alibaba pase index

---
+++ docs/alibaba_pase/01_init.sql
@@ -0,0 +1,3 @@
+create extension pase;
+set search_path to 'information_schema';
+create table public.sift_vectors (vector float4[]);
---
+++ docs/alibaba_pase/02_fill_table.py
@@ -0,0 +1,39 @@
+import psycopg2
+import numpy as np
+
+conn = psycopg2.connect(dbname='***', user='***', port=1921, password='***', host='***')
+
+
+def fvecs_read(filename):
+    fv = np.fromfile(filename, dtype=np.float32)
+    if fv.size == 0:
+        return np.zeros((0, 0))
+    dim = fv.view(np.int32)[0]
+    assert dim > 0
+    fv = fv.reshape(-1, 1 + dim)
+    if not all(fv.view(np.int32)[:, 0] == dim):
+        raise IOError("Non-uniform vector sizes in " + filename)
+    fv = fv[:, 1:]
+    fv = fv.copy()
+    return fv
+
+
+def fill_table_query():
+    query = "INSERT INTO public.sift_vectors (vector) VALUES "
+    i = 0
+    for vec in fvecs_read("../../test/test_data/sift/sift_base.fvecs"):
+        query += (',' if i % 10000 != 0 else "") + "('{" + ",".join(vec.astype(str)) + "}')"
+        i += 1
+        if i % 10000 == 0 and i != 0:
+            query += ";"
+            cursor.execute(query)
+            conn.commit()
+            query = "INSERT INTO public.sift_vectors (vector) VALUES "
+            print(f"Added {i} vectors")
+
+
+cursor = conn.cursor()
+cursor.execute("set search_path to 'information_schema';")
+
+fill_table_query()
+conn.commit()
